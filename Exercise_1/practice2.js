d3c85aeea3a6f6c1c3719998a14f0ce89aac0d68
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-01-11 12:10:26 +0100

    ..

--- Exercise_1/practice2.js
+++
@@ -1,9 +0,0 @@
-let array = [1,3,4, 1, 4, 5, 6, 3, 2, 8, 2, 1];
-function remove(array, index){
-    //slice from beginning to index(to remove index)
-    return array.slice(0, index)
-    //concat another slice from index + 1 to end
-    .concat(array.slice(index + 1));
-}
-
-console.log(remove(array, 6));
